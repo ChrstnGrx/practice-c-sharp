ae6ed93796ac417d610a3e6bfee562d98d69298b
ChrstnGrx <christinegroux1@gmail.com>
2022-02-03 12:56:38 -0500

    First version of Number Guesser App

--- Csharp-practice/Program.cs
+++ Csharp-practice/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Csharp_practice
 {
@@ -10,7 +6,75 @@ namespace Csharp_practice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            GetAppInfo();
+
+            GreetUser();
+
+            while (true) {
+                Random random = new Random();
+                int correctNumber = random.Next(1, 10);
+                int guessNumber = 0;
+
+                Console.WriteLine("Guess a number between 1 and 10.");
+
+                while (guessNumber != correctNumber)
+                {
+                    string playerInput = Console.ReadLine();
+                    if (!int.TryParse(playerInput, out guessNumber))
+                    {
+                        PrintColorMessage("Sorry, not a number!", ConsoleColor.Red);
+                        continue;
+                    }
+                    guessNumber = Int32.Parse(playerInput);
+
+                    if (guessNumber != correctNumber)
+                    {
+                        PrintColorMessage("Wrong number :( Please Try again.", ConsoleColor.Red);
+                    }
+                }
+
+                PrintColorMessage("Success! You won!", ConsoleColor.Yellow);
+
+                Console.WriteLine("Do you want to play again? [Y or N]");
+                string answer = Console.ReadLine().ToUpper();
+                if (answer == "Y")
+                {
+                    continue;
+                }
+                else if (answer == "N")
+                {
+                    return;
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Christine Groux";
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+            Console.ResetColor(); // white
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your Name?");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("Let's play a game, {0}!", playerName);
+        }
+
+        static void PrintColorMessage(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor(); // white
         }
     }
 }
